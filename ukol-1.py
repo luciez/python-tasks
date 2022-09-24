119235440cdd87eeef10c477c256f6d5bbe9e3c6
Lucie <zavrelova.l@gmail.com>
2022-09-24 13:57:14 +0200

    First task - Dictionary with conditions

---
+++ ukol-1.py
@@ -0,0 +1,15 @@
+baliky = {
+    "B541X": True,
+    "B547X": False,
+    "B251X": False,
+    "B501X": True,
+    "B947X": False,
+}
+balik=input("Zadej kod baliku:")
+if balik in baliky:
+    if baliky[balik]== True:
+        print ("Balík byl předán kurýrovi")
+    else:
+        print ("Balík zatím nebyl předán kurýrovi")
+else:
+    print ("Balík nebyl nalezen")
