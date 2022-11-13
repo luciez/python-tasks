1e18671d028f0fb4a882e8ef6396195fc8fddb0d
Lucie <zavrelova.l@gmail.com>
2022-11-13 22:41:00 +0100

    pridan replace a upravená flow

--- ukol-7.py
+++ ukol-7.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta, date, time, tzinfo
+from datetime import datetime
 
 premium_od=datetime(2021, 7, 1)
 premium_do=datetime(2021, 8, 10)
@@ -7,11 +7,8 @@ lowcost_od=datetime(2021, 8, 11)
 lowcost_do=datetime(2021, 8, 31)
 lowcost_cena=180
 
-pozadovane_datum=input("Kdy plánuješ kino navštívit: ")
-pocet_vstupenek=int(input("Kolik vstupenek kupuješ: "))
-
 def vytvor_datetime(datum):
-    return datetime.strptime(datum, "%d. %m. %Y")
+    return datetime.strptime(datum, "%d.%m.%Y")
 
 def vrat_cenu_vstupenky(datum_navstevy):
     if datum_navstevy >= premium_od and datum_navstevy <= premium_do:
@@ -19,16 +16,16 @@ def vrat_cenu_vstupenky(datum_navstevy):
     elif datum_navstevy >= lowcost_od and datum_navstevy <= lowcost_do:
         return lowcost_cena
     else:
-        return 0
-
-def vypocet_ceny(cena_za_kus, pocet_vstupenek):
-    if cena_za_kus==0:
         print("Tento den nehrajeme. Vyber si datum mezi 1. 7. a 31. 8. 2021.")
-    else:
+        exit()
+
+def zobraz_konecnou_cenu(cena_za_kus, pocet_vstupenek):
         print(f"Zaplatíš celkem {cena_za_kus*pocet_vstupenek} Kč.")
 
+pozadovane_datum=(input("Kdy plánuješ kino navštívit: ")).replace(" ","")
 datum_navstevy=vytvor_datetime(pozadovane_datum)
 cena_za_kus=vrat_cenu_vstupenky(datum_navstevy)
-vypocet_ceny(cena_za_kus, pocet_vstupenek)
+pocet_vstupenek=int(input("Kolik vstupenek kupuješ: "))
+zobraz_konecnou_cenu(cena_za_kus, pocet_vstupenek)
 
 
